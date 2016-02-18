4c3bee2bbad587a9910bed27313e22340e90a325
Demis Bellot <demis.bellot@gmail.com>
2016-02-18 17:02:53 -0500

    Add Signed version of ServiceStack.Admin

--- build/build.bat
+++ build/build.bat
@@ -1,5 +1,7 @@
 SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 REM %MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0
+REM %MSBUILD% build-sn.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=0
+REM exit
 
 cd ..\src\ServiceStack.Admin.WebHost
 
--- lib/copy.bat
+++ lib/copy.bat
@@ -2,5 +2,5 @@ COPY C:\src\ServiceStack.OrmLite\src\ServiceStack.OrmLite.Sqlite\bin\Release\* .
 COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\Release\* .
 REM COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\Debug\* .
 
-REM SET BUILD=Debug
-SET BUILD=Release
+COPY C:\src\ServiceStack\lib\signed\* signed\
+
