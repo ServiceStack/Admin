1e49b160b2d40f8ca80aaf911215658355c7c040
Demis Bellot <demis.bellot@gmail.com>
2016-04-13 20:00:32 -0400

    Add example of AutoQueryData Service returning and querying CSV logs

--- src/ServiceStack.Admin.WebHost/Global.asax.cs
+++ src/ServiceStack.Admin.WebHost/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Funq;
 using ServiceStack.Data;
 using ServiceStack.OrmLite;
@@ -15,9 +16,9 @@ namespace ServiceStack.Admin.WebHost
             container.Register<IDbConnectionFactory>(c =>
                 new OrmLiteConnectionFactory("~/App_Data/db.sqlite".MapHostAbsolutePath(), SqliteDialect.Provider));
 
-            Plugins.Add(new AutoQueryFeature {
-                MaxLimit = 100
-            });
+            Plugins.Add(new AutoQueryFeature { MaxLimit = 100 });
+
+            Plugins.Add(new AutoQueryDataFeature { MaxLimit = 100 });
         }
     }
 
@@ -35,6 +36,42 @@ namespace ServiceStack.Admin.WebHost
             return new HttpResult(VirtualFileSources.GetFile("index.html"));
         }
     }
+
+
+    [Route("/query/requestlogs")]
+    [Route("/query/requestlogs/{Date}")]
+    public class QueryRequestLogs : QueryData<RequestLogEntry>
+    {
+        public DateTime? Date { get; set; }
+        public bool ViewErrors { get; set; }
+    }
+
+    public class AutoQueryDataServices : Service
+    {
+        public IAutoQueryData AutoQuery { get; set; }
+
+        public object Any(QueryRequestLogs query)
+        {
+            var date = query.Date.GetValueOrDefault(new DateTime(2016,4,13));
+            var logSuffix = query.ViewErrors ? "-errors" : "";
+            var csvLogsFile = VirtualFileSources.GetFile("requestlogs/{0}-{1}/{0}-{1}-{2}{3}.csv".Fmt(
+                date.Year.ToString("0000"),
+                date.Month.ToString("00"),
+                date.Day.ToString("00"),
+                logSuffix));
+
+            if (csvLogsFile == null)
+                throw HttpError.NotFound("No logs found on " + date.ToShortDateString());
+
+            var logs = csvLogsFile.ReadAllText().FromCsv<List<RequestLogEntry>>();
+
+            var q = AutoQuery.CreateQuery(query, Request,
+                db: new MemoryDataSource<RequestLogEntry>(logs, query, Request));
+
+            return AutoQuery.Execute(query, q);
+        }
+    }
+
 
     public class Global : System.Web.HttpApplication
     {
