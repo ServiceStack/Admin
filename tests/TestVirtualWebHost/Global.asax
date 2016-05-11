c0cc59eec00091c2702f1c5d492698395a883b5b
Demis Bellot <demis.bellot@gmail.com>
2016-05-11 18:23:00 -0400

    Add TestVirtualWebHost test project to test hosting on Virtual directory at custom /api path

---
+++ tests/TestVirtualWebHost/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestVirtualWebHost.Global" Language="C#" %>
---
+++ tests/TestVirtualWebHost/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Funq;
+using ServiceStack;
+using ServiceStack.Admin;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+
+namespace TestVirtualWebHost
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("Virtual WebHost Admin UI", typeof(AutoQueryServices).Assembly) { }
+
+        public override void Configure(Container container)
+        {
+            container.Register<IDbConnectionFactory>(c =>
+                new OrmLiteConnectionFactory("~/App_Data/db.sqlite".MapHostAbsolutePath(), SqliteDialect.Provider));
+
+            Plugins.Add(new AutoQueryFeature
+            {
+                MaxLimit = 100
+            });
+
+            Plugins.Add(new AdminFeature());
+
+            SetConfig(new HostConfig {
+                HandlerFactoryPath = "api",
+                WebHostUrl = "http://localhost/TestVirtual.WebHost/api",
+            });
+        }
+    }
+
+    public class AutoQueryServices : Service
+    {
+    }
+
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new AppHost().Init();
+        }
+    }
+}
