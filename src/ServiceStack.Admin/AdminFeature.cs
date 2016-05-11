7eb0267606a05b262df788a08b0eb34b2d24274b
Demis Bellot <demis.bellot@gmail.com>
2016-05-11 18:51:22 -0400

    Change static paths at runtime to ~/ss_admin to support virtual web directories

--- src/ServiceStack.Admin/AdminFeature.cs
+++ src/ServiceStack.Admin/AdminFeature.cs
@@ -13,15 +13,14 @@ namespace ServiceStack.Admin
         public void Register(IAppHost appHost)
         {
             var indexHtml = appHost.VirtualFileSources.GetFile("ss_admin/index.html").ReadAllText();
-            if (appHost.Config.WebHostUrl != null)
-                indexHtml = indexHtml.Replace("/ss_admin", appHost.Config.WebHostUrl.CombineWith("ss_admin"));
-            else if (appHost.Config.HandlerFactoryPath != null)
-                indexHtml = indexHtml.Replace("/ss_admin", "/{0}/ss_admin".Fmt(appHost.Config.HandlerFactoryPath));
 
             appHost.CatchAllHandlers.Add((httpMethod, pathInfo, filePath) => 
                 pathInfo.StartsWith("/ss_admin") 
                     ? (pathInfo == "/ss_admin/index.html" || !appHost.VirtualFileSources.FileExists(pathInfo)
-                        ? new StaticContentHandler(indexHtml, MimeTypes.Html) as IHttpHandler
+                        ? new CustomActionHandler((req, res) => {
+                            res.ContentType = MimeTypes.Html;
+                            res.Write(indexHtml.Replace("/ss_admin", req.ResolveAbsoluteUrl("~/ss_admin")));
+                        }) as IHttpHandler
                         : new StaticFileHandler(appHost.VirtualFileSources.GetFile(pathInfo)))
                     : null);
 
