9e12416da9cb98eda0eba2137514fb02a1270483
Demis Bellot <demis.bellot@gmail.com>
2017-12-15 02:22:33 -0500

    Call UpdateVersion before build + remove unused files

--- build/build.bat
+++ build/build.bat
@@ -1,8 +1,6 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
-REM %MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
-REM %MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
-REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;PatchVersion=9
+REM %MSBUILD% build.proj /target:Default;NuGetPack /property:Configuration=Release;MinorVersion=1;PatchVersion=0
 REM exit
 
 PUSHD ..\src\ServiceStack.Admin.Web
