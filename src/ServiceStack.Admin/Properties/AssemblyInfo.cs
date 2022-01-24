3392ffa8a598c52d2f0ddd6a4c40cbbb7f143a04
Demis Bellot <demis.bellot@gmail.com>
2022-01-25 00:59:24 +0800

    Upgrade to v6

--- src/ServiceStack.Admin/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Admin/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("c6eb693e-64d1-4271-be68-0b78d186003e")]
-[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("6.0.0.0")]
